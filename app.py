9b9c2e7f7e2b59a57d8c9a31289096fd6ca99ed0
Tharrun S <96402565+TheDunkinDude@users.noreply.github.com>
2023-11-28 12:50:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -92,4 +92,4 @@ def submit_data():
         return jsonify({'error': 'Failed to submit data'}), 500'''
 
 if __name__ == "__main__":
-    app.run(host='localhost' , port = 5000,debug=True)
+    app.run(port = 8000)
